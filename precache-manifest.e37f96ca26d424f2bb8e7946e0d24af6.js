ecb5a7d1e3cdf2918d44eea752ba7c76b43e19a6
Djalmir <djalmir_miodutzki@hotmail.com>
2021-08-19 21:45:40 -0300

    update

---
+++ precache-manifest.e37f96ca26d424f2bb8e7946e0d24af6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434f7cb8be684bef680c",
+    "url": "/TaskControlApp/css/app.5fdedb24.css"
+  },
+  {
+    "revision": "83ac8b701d17e829ddeb77bd5cffaf87",
+    "url": "/TaskControlApp/img/add.83ac8b70.svg"
+  },
+  {
+    "revision": "df0f669ea0e782b7bf3c9695f7f8a5ec",
+    "url": "/TaskControlApp/img/cancel.df0f669e.svg"
+  },
+  {
+    "revision": "334a307e3c9836a439a204b791cc3304",
+    "url": "/TaskControlApp/img/done.334a307e.svg"
+  },
+  {
+    "revision": "cab2c13c593d07efe30f69e8a7244bac",
+    "url": "/TaskControlApp/img/edit.cab2c13c.svg"
+  },
+  {
+    "revision": "6cae157db226f38c0c9021a43e4fffea",
+    "url": "/TaskControlApp/img/loading.6cae157d.svg"
+  },
+  {
+    "revision": "b7a5534e70c2e85a6dc6c94beb14964b",
+    "url": "/TaskControlApp/img/menu.b7a5534e.svg"
+  },
+  {
+    "revision": "c1845fb41daadf4936fb1c877bb412da",
+    "url": "/TaskControlApp/img/trash.c1845fb4.svg"
+  },
+  {
+    "revision": "071048fbf55bf0530844b26e9e06e412",
+    "url": "/TaskControlApp/index.html"
+  },
+  {
+    "revision": "434f7cb8be684bef680c",
+    "url": "/TaskControlApp/js/app.0ad001e3.js"
+  },
+  {
+    "revision": "ba9757f97cfbabd2c648",
+    "url": "/TaskControlApp/js/chunk-vendors.79421db6.js"
+  },
+  {
+    "revision": "6587b39c74cf4c5a76641dde6d8adae7",
+    "url": "/TaskControlApp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TaskControlApp/robots.txt"
+  }
+]);
